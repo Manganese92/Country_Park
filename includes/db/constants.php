1d113be443ffa32ef78aee15a1ce70e26da8a513
WILLY Falone <willyfkouadio@gmail.com>
2022-08-21 02:16:33 +0200

    crée la structure du projet
    mise en place page home

---
+++ includes/db/constants.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_DRIVER", "mysql");
+define("DB_USER", "root");
+define("DB_PWD", "");
+define("DB_HOST", "localhost");
+define("DB_PORT", "3306");
+define("DB_NAME", "country_park_db");
+?>
---
+++ includes/sections/navbar.php
@@ -0,0 +1,28 @@
+<header class="p-3 mb-3 border-bottom">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="index.php" class="text-dark text-decoration-none">
+                <span class="fs-4">Country Park</span>
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            </ul>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+            <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+        </form>
+
+            <div class="dropdown text-end">
+                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/willf80.png" alt="willf80" width="32" height="32" class="rounded-circle">
+                </a>
+                <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+                    <li><a class="dropdown-item fw-bold" href="inscription.php">Inscription</a></li>
+                    <li><a class="dropdown-item" href="connexion.php">Connexion</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">A propos</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
