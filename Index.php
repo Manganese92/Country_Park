8495e7c799452a0e25ea86b40fd1f5850626d1de
WILLY Falone <willyfkouadio@gmail.com>
2022-08-21 10:53:12 +0200

    formatage des cards

--- Index.php
+++ Index.php
@@ -8,8 +8,6 @@ include 'includes/sections/navbar.php';
 ?>
 
 <div class="container">
-  <!-- <h1>Liste des biens</h1> -->
-
   <div class="row row-cols-1 row-cols-lg-4 row-cols-md-3">
     
     <?php
@@ -19,12 +17,11 @@ include 'includes/sections/navbar.php';
         <div class="card h-100">
           <img src="https://a0.muscache.com/im/pictures/333e3ef0-9334-4d9b-95e8-622db0216afe.jpg?im_w=720" class="card-img-top" alt="...">
           <div class="card-body">
-            <h6 class="card-title text-truncate"><?= $bien['libelle'] ?></h6>
-            <p class="card-text p-0 m-0">Mobile home</p>
-            <p class="card-text p-0 m-0"><?= $bien['datedebut'].' - '.$bien['datefin'] ?></p>
-            <p class="card-text">
-            <span class="text-success"><?= $bien['prix'] ?> €</span>
-            nuit
+            <p class="card-title text-truncate mb-0 fw-bold p-0"><?= $bien['libelle'] ?></p>
+            <p class="card-subtitle p-0 m-0">Mobile home</p>
+            <p class="text-muted p-0 m-0"><?= $bien['datedebut'].' - '.$bien['datefin'] ?></p>
+            <p class="text-muted p-0 m-0">
+            <span class="text-dark"><?= $bien['prix'] ?> €</span> nuit
             </p>
           </div>
         </div>
