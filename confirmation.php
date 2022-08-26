b2a496452e04bac4f15aecca329e2ae5160868bb
Morgane Regnaut <mregnaut.esgi@gmail.com>
2022-08-26 14:17:45 +0200

    fin de l'envoi de mail

--- confirmation.php
+++ confirmation.php
@@ -10,29 +10,23 @@ $pdo = get_connexion_pdo();
 $nom = $_GET['nom'];
 $cle = $_GET['cle'];
 
-// Récupération de la clé correspondant au $login dans la base de données
-$stmt = $pdo->prepare("SELECT cle,actif FROM utilisateurs WHERE nom like :nom ");
-if ($stmt->execute(array(':nom' => $nom)) && $row = $stmt->fetch()) {
-    $clebdd = $row['cle'];    // Récupération de la clé
-    $actif = $row['actif']; // $actif contiendra alors 0 ou 1
-}
-
-
-// On teste la valeur de la variable $actif récupérée dans la BDD
-if ($actif == '1'){
-    echo "Votre compte est déjà actif !";
-} else {
-    if ($cle == $clebdd) // On compare nos deux clés    
-    {
+// Récupération de la clé correspondant au compte dans la base de données
+$stmt = $pdo->prepare("SELECT * FROM utilisateurs WHERE cle = :cle ");
+if ($stmt->execute(array(':cle' => $cle)) && $row = $stmt->fetch()) {
+    $actif = $row['statut']; // $actif contiendra alors 0 ou 1
+    // On teste la valeur de la variable $actif récupérée dans la BDD
+    if ($actif == '1') {
+        echo "Votre compte est déjà actif !";
+    } else {
         // Si elles correspondent on active le compte !    
         echo "Votre compte a bien été activé !";
 
         // La requête qui va passer notre champ actif de 0 à 1
-        $stmt = $pdo->prepare("UPDATE utilisateurs SET actif = 1 WHERE nom like :nom ");
-        $stmt->bindParam(':nom', $nom);
+        $stmt = $pdo->prepare("UPDATE utilisateurs SET statut = 1 WHERE email like :email ");
+        $stmt->bindParam(':email', $email);
         $stmt->execute();
-    } else // Si les deux clés sont différentes on provoque une erreur...
-    {
-        echo "Erreur ! Votre compte ne peut être activé...";
     }
+} else // Si les deux clés sont différentes on provoque une erreur...
+{
+    echo "Erreur ! Votre compte ne peut être activé...";
 }
