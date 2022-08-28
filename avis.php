798f1705a4495a0c2d7ae3ea8f25c2c082cf5fa8
Morgane Regnaut <mregnaut.esgi@gmail.com>
2022-08-28 22:41:53 +0200

    correction des erreurs profils

---
+++ avis.php
@@ -0,0 +1,24 @@
+<div id="makeThreadCommentBody" class="shadow">
+    <div class="container text-center">
+        <form id="userThreadComment" method="POST" action="./addThreadComment.php?current=<?php echo $current ?>" enctype="multipart/form-data" class="pt-2">
+            <div class="row pt-3">
+                <h5 class="formTitles">Commentaire</h5>
+                <div class="col"></div>
+                <div class="col-10">
+                    <div class="row" style="height: 100px;">
+                        <textarea form="userThreadComment" name="userComment" placeholder="..." style="border-radius: 20px;"></textarea>
+                    </div>
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="row pt-2">
+            <div class="row">
+            <div class="col"></div>
+                <div class="col">
+                    <input type="submit" class="btn btn-outline-danger mt-4" value="Crée">
+                </div>
+            <div class="col"></div>
+            </div>
+        </form>
+    </div>
+</div>
--- inscription_valid.php
+++ inscription_valid.php
@@ -4,30 +4,24 @@ require_once 'includes/page_header.php';
 require_once 'includes/sections/navbar.php';
 ?>
 
-<div class=" bg-secondary">
-<div class="container">
-  <div class="mb-sm-4 d-lg-none" style="padding-bottom: 100px;"></div>
-  <div class="mb-sm-4 d-block" style="padding-bottom: 80px;"></div>
-  <div class="row row-cols-1 row-cols-lg-4 row-cols-md-3">
-</div>
-
-
-
-
-<div class="modal modal-sheet position-static d-block " tabindex="-1" role="dialog" id="modalSheet">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content rounded-4 ">
-      <div class="modal-header border-bottom-0">
-      </div>
-      <div class="modal-body py-0">
-        <p>Un mail de confirmation vous a été envoyé. Veuillez vérifier votre boite email</p>
-      </div>
-      <div class="modal-footer flex-column border-top-0">
+<div class=" bg-secondary vh-100">
+  <div class="container">
+    <div class="mb-sm-4 d-lg-none" style="padding-bottom: 100px;"></div>
+    <div class="mb-sm-4 d-block" style="padding-bottom: 80px;"></div>
+    <div class="modal modal-sheet position-static d-block " tabindex="-1" role="dialog" id="modalSheet">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content rounded-4 ">
+          <div class="modal-header border-bottom-0">
+          </div>
+          <div class="modal-body py-0">
+            <p>Un mail de confirmation vous a été envoyé. Veuillez vérifier votre boite email</p>
+          </div>
+          <div class="modal-footer flex-column border-top-0">
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
-</div>
 
 
-<?php require 'includes/page_footer.php' ?>
+  <?php require 'includes/page_footer.php' ?>
