252674d6b67edfae854549e7fbb46731bb8c54b1
WILLY Falone <willyfkouadio@gmail.com>
2022-08-29 12:10:09 +0200

    Gestion des erreurs global 404

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+require 'includes/page_header.php';
+require 'includes/sections/navbar.php';
+require_once 'includes/db/utilisateurs.sql.php';
+?>
+
+<div>
+    <div class="container text-center">
+        <div class="mb-sm-4 d-lg-none" style="padding-bottom: 100px;"></div>
+        <div class="mb-sm-4 d-block" style="padding-bottom: 80px;"></div>
+
+        <h2 class="text-center">Page introuvable !</h2>
+
+        <a href="index.php" class="text-dark d-block mt-4">Retour à la page d'accueil</a>
+    </div>
+</div>
+
+<?php require 'includes/page_footer.php' ?>
